08d2a073bb6bea2d95ed147f21a15462bd208837
icoreman <296328799@qq.com>
2019-05-12 22:00:22 +0800

    这是传统的代码生成器方法
    接下来改为使用 MyBatis-Plus

---
+++ spike-dao/src/main/java/org/mybatis/generator/plugins/RenameJavaMapperPlugins.java
@@ -0,0 +1,60 @@
+package org.mybatis.generator.plugins;
+
+import static org.mybatis.generator.internal.util.StringUtility.stringHasValue;
+import static org.mybatis.generator.internal.util.messages.Messages.getString;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+
+/**
+ * @ClassName: RenameMapperPlugins
+ *
+ * @author xuxx
+ * @date 2019-05-12 16:12:45
+ * @since JDK 1.8
+ *
+ */
+public class RenameJavaMapperPlugins extends PluginAdapter {
+
+	private String searchString;
+	private String replaceString;
+	private Pattern pattern;
+
+	public boolean validate(List<String> warnings) {
+
+		searchString = properties.getProperty("searchString");
+		replaceString = properties.getProperty("replaceString");
+
+		boolean valid = stringHasValue(searchString)
+				&& stringHasValue(replaceString);
+
+		if (valid) {
+			pattern = Pattern.compile(searchString);
+		} else {
+			if (!stringHasValue(searchString)) {
+				warnings.add(getString("ValidationError.18",
+						"RenameExampleClassPlugin",
+						"searchString"));
+			}
+			if (!stringHasValue(replaceString)) {
+				warnings.add(getString("ValidationError.18",
+						"RenameExampleClassPlugin",
+						"replaceString"));
+			}
+		}
+		return valid;
+	}
+
+	@Override
+	public void initialized(IntrospectedTable introspectedTable) {
+		String oldType = introspectedTable.getMyBatis3JavaMapperType();
+		Matcher matcher = pattern.matcher(oldType);
+		oldType = matcher.replaceAll(replaceString);
+
+		introspectedTable.setMyBatis3JavaMapperType(oldType);
+	}
+}
